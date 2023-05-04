2f67e9fd00eafc3c66c179ffabab88f621a014a4
trentnixon <trentnixon@gmail.com>
2023-05-04 15:28:53 +1000

    new add

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
 
-import { Inter } from "@next/font/google";
+//import { Inter } from "@next/font/google";
 import { useRouter } from "next/router";
 import { AuthenticationImage } from "../components/Login/Login";
 
 import { getFakeToken } from "../lib/auth";
 import { Public } from "../components/template/PublicShell";
-const inter = Inter({ subsets: ["latin"] });
+//const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [render, setrender] = useState(false);
@@ -28,27 +28,6 @@ export default function Home() {
     </Public>
   );
 }
-
-/*
-
-export const getServerSideProps = async (context) => {
-  const response = await fetcher(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/associations`
-  );
-  const CaseStudies = await fetcher(
-    `${process.env.NEXT_PUBLIC_STRAPI_URL}/case-studies?${CS_query}`
-  );
-  //
- 
-//console.log(response);
-  return {
-    props: {
-      associations: response,
-      CaseStudies:CaseStudies
-    },
-  };
-};
-*/
 
 /*
   git add .
