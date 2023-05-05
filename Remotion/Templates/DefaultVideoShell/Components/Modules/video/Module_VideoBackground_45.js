1e4af2ccbd7275d00b63a934d3bfda9bddc3d7bb
trentnixon <trentnixon@gmail.com>
2023-05-05 13:07:42 +1000

    remove remotion

--- Remotion/Templates/DefaultVideoShell/Components/Modules/video/Module_VideoBackground_45.js
+++ Remotion/Templates/DefaultVideoShell/Components/Modules/video/Module_VideoBackground_45.js
@@ -4,10 +4,10 @@ import {ModuleVideoBackgroundNoText45} from './Sequences/VideoBackground_NoText'
 import {ModuleVideoBackgroundSingleText45} from './Sequences/VideoBackground_SingleText'
 import {ModuleVideoBackgroundTitleSubtitle45} from './Sequences/VideoBackground_Title_Subtitle'
 import {ModuleVideoBackgroundLogoStaticOfferDisclamer45} from './Sequences/VideoBackground_Logo_StaticOffer_Disclamer'
-//import {ModuleVideoBackgroundLogoOfferDisclamer45} from './Sequences/VideoBackground_Logo_Prefix_Offer_Suffix_Disclamer'
+import {ModuleVideoBackgroundLogoPrefixOfferSuffixDisclamer45} from './Sequences/VideoBackground_Logo_Prefix_Offer_Suffix_Disclamer'
 // Dynamic
 
-  
+
 
 // Export Modules
 // Static
@@ -15,7 +15,7 @@ export const ModuleVideoBackgroundNoText = ({DATA, Duration}) => ModuleVideoBack
 export const ModuleVideoBackgroundTitleSubtitle = ({DATA, Duration}) => ModuleVideoBackgroundTitleSubtitle45({DATA, Duration}) 
 export const ModuleVideoBackgroundSingleText = ({DATA, Duration}) => ModuleVideoBackgroundSingleText45({DATA, Duration}) 
 export const ModuleVideoBackgroundLogoStaticOfferDisclamer = ({DATA, Duration})=> ModuleVideoBackgroundLogoStaticOfferDisclamer45({DATA, Duration});
-//export const ModuleVideoBackgroundLogoPrefixOfferSuffixDisclamer = ({DATA, Duration})=> ModuleVideoBackgroundLogoOfferDisclamer45({DATA, Duration});
+export const ModuleVideoBackgroundLogoPrefixOfferSuffixDisclamer = ({DATA, Duration})=> ModuleVideoBackgroundLogoPrefixOfferSuffixDisclamer45({DATA, Duration});
 
 
 // Dynamic
